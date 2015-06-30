fe3ebb774c5c18d81a820c063b0c11d961ea6594
Emmanuel Atse <emmanuel.atse@gmail.com>
2015-06-30 18:33:59 +0200

    Added praytimes.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # prayertime-fr
 Accurate prayer time calculation for France
+
+Using PrayTimes.js from http://praytimes.org
--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,11 @@ var uglify     = require('gulp-uglify');
 var concat     = require('gulp-concat');
 
 gulp.task('default', function(){
-
+  var js = gulp.src(['praytimes.js'])
+    .pipe(uglify({preserveComments:'some'}))
+    .pipe(concat('prayertime-fr.min.js'))
+    .pipe(gulp.dest('./'));
+  js = gulp.src(['praytimes.js'])
+    .pipe(concat('prayertime-fr.js'))
+    .pipe(gulp.dest('./'));
 });
