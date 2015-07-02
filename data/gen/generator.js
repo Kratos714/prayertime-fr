239f4e7ca7251d37562f64614fe75b808d2ae2a1
Emmanuel Atse <emmanuel.atse@gmail.com>
2015-07-02 17:52:58 +0200

    Changed variable name for data in js

---
+++ data/gen/generator.js
@@ -0,0 +1,40 @@
+var util = require('util');
+
+function add_city_data(city, data, name, lat, lon){
+  var obj = {
+    city : name,
+    latitude : lat,
+    longitude : lon,
+    times : []
+  };
+  var month = [];
+
+  for (var i = 0; i < city.length; i += 6){
+    if (city[i] === "1" && !(month.length == 0)){
+      obj.times.push(month);
+      month = [];
+    }
+    month.push(city[i + 1]);
+  }
+  obj.times.push(month);
+  data.push(obj);
+}
+
+var prayertime_fr_data = [];
+add_city_data(prayertime_fr_amiens, prayertime_fr_data, "amiens", 49.90095321859007, 2.290074455386684);
+add_city_data(prayertime_fr_besancon, prayertime_fr_data, "besancon", 47.25538722494024, 6.0194869649423115);
+add_city_data(prayertime_fr_bordeaux, prayertime_fr_data, "bordeaux", 44.85724453514966, 0.5736967811598869);
+add_city_data(prayertime_fr_clermont_ferrand, prayertime_fr_data, "clermont_ferrand", 45.785649299085584, 3.1155454290268803);
+add_city_data(prayertime_fr_lille, prayertime_fr_data, "lille", 50.631718316778176, 3.0478327231208246);
+add_city_data(prayertime_fr_lyon, prayertime_fr_data, "lyon", 45.7699284396584, 4.829224649781766);
+add_city_data(prayertime_fr_marseille, prayertime_fr_data, "marseille", 43.29990094363675, 5.382278697952184);
+add_city_data(prayertime_fr_metz, prayertime_fr_data, "metz", 49.10811332792492, 6.19552454210356);
+add_city_data(prayertime_fr_mulhouse, prayertime_fr_data, "mulhouse", 47.749163302979674, 7.325700475094066);
+add_city_data(prayertime_fr_orleans, prayertime_fr_data, "orleans", 47.882863421380264, 1.9161035747737603);
+add_city_data(prayertime_fr_paris, prayertime_fr_data, "paris", 48.8626304851685, 2.3362934465505396);
+add_city_data(prayertime_fr_poitiers, prayertime_fr_data, "poitiers", 46.583920772572576, 0.35994765300316445);
+add_city_data(prayertime_fr_rouen, prayertime_fr_data, "rouen", 49.44134601033831, 1.0925678427798247);
+add_city_data(prayertime_fr_strasbourg, prayertime_fr_data, "strasbourg", 48.571267984911756, 7.767526795169564);
+add_city_data(prayertime_fr_toulouse, prayertime_fr_data, "toulouse", 43.59638143032458, 1.4316729336369596);
+add_city_data(prayertime_fr_tours, prayertime_fr_data, "tours", 47.39863822805879, 0.6965263764166114);
+console.log('var prayertime_fr_data = ' + JSON.stringify(prayertime_fr_data, null, 2) + ';');
