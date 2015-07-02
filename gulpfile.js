8791f296a4c51755f9c4e55aea856f123136ae69
Emmanuel Atse <emmanuel.atse@gmail.com>
2015-07-02 16:16:55 +0200

    Fixed some issues with raw data

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,12 @@ var uglify     = require('gulp-uglify');
 var concat     = require('gulp-concat');
 
 gulp.task('default', function(){
-  var js = gulp.src(['praytimes.js'])
+  var srcs = ['praytimes.js', 'data/js/*.js'];
+  var js = gulp.src(srcs)
     .pipe(uglify({preserveComments:'some'}))
     .pipe(concat('prayertime-fr.min.js'))
     .pipe(gulp.dest('./'));
-  js = gulp.src(['praytimes.js'])
+  js = gulp.src(srcs)
     .pipe(concat('prayertime-fr.js'))
     .pipe(gulp.dest('./'));
 });
